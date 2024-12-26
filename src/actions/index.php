1a35d0376ae35e7051c2d34ac108bd22eed34185
wangchao <zjsxwc@qq.com>
2024-12-26 10:15:16 +0800

    fix

--- src/actions/index.php
+++ src/actions/index.php
@@ -3,6 +3,36 @@ use Workerman\Protocols\Http\Response;
 use Workerman\Protocols\Http\Request;
 
 function index(Request $request) {
+    global $validCredentials;
+
+    // 获取请求头中的 Authorization 信息
+    $auth_header = $request->header('Authorization');
+    if (!$auth_header) {
+        // 发送 401 Unauthorized 响应，并要求客户端进行身份验证
+        $response = new Response(401);
+        $response->withHeader('WWW-Authenticate', 'Basic realm="Restricted Area"');
+        return $response;
+    }
+
+    // 解析 Authorization 信息
+    list($auth_type, $auth_data) = explode(' ', $auth_header);
+    if ($auth_type === 'Basic') {
+        $decoded = base64_decode($auth_data);
+        list($username, $password) = explode(':', $decoded);
+        // 验证用户名和密码
+        if (isset($validCredentials[$username]) && $validCredentials[$username] === $password) {
+            // 身份验证成功，继续处理请求
+            goto INDEX;
+        } else {
+            // 身份验证失败，发送 403 Forbidden 响应
+            return new Response(403);
+        }
+    } else {
+        // 不支持的身份验证类型，发送 400 Bad Request 响应
+        return new Response(400);
+    }
+
+    INDEX:
     // 创建一个响应对象
     $response = new Response();
     // 设置响应头，这里设置内容类型为text/html
--- src/parameters.php
+++ src/parameters.php
@@ -3,3 +3,6 @@
 $windowTitle = "Wine Desktop";
 $win32exePath = __DIR__ . "/../../win32ExeDir/win32.exe";
 $startWindowCmd = "/usr/bin/wine " . $win32exePath;
+$validCredentials = [
+    "tester" => "pwd123456"
+];
